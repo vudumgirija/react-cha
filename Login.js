501e97c9b5392c2069cdad3ca875d154a01ba2cb
vudumgirija <103623179+vudumgirija@users.noreply.github.com>
2022-05-19 11:27:04 +0530

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,7 +1,23 @@
-import React from'react'
-export default function Login(){
-    return(
-        <>
-        </>
-    )
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+      <TextField id="outlined-basic" label="password" variant="outlined" />
+      <TextField id="outlined-basic" label="conform password" variant="outlined" />
+      <Button variant="contained">Login</Button>
+    </Box>
+    
+  );
 }
