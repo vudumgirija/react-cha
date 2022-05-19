47db40a9722ba5d83511cd273b16a35eae684df8
vudumgirija <103623179+vudumgirija@users.noreply.github.com>
2022-05-19 11:04:07 +0530

    Add files via upload

---
+++ Posts.js
@@ -0,0 +1,6 @@
+import React from'react'
+export default function Posts(){
+    return(
+        <h1>This is posts page</h1>
+    )
+}
---
+++ Profile.js
@@ -0,0 +1,6 @@
+import React from'react'
+export default function Profile(){
+    return(
+        <h1>This is Profile page</h1>
+    )
+}
